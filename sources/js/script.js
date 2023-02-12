ffdf97187aeb61c64d97d28e1f6b60f9de34c29a
Szczapa <98310905+Szczapa@users.noreply.github.com>
2023-02-12 10:16:24 +0100

    Revert "Update script.js"
    This reverts commit 2ae35a6666a489b4b1ff5ed94ed4c9d34d439e75.

--- sources/js/script.js
+++ sources/js/script.js
@@ -148,16 +148,6 @@ const editMyProductInt = async (productId) => {
             required
             />
             </div>
-            <div class="col-12">             
-                <input class="form-control" type="file" id="formFile">            
-            </div>
-            <div class="col-12">             
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Selectionner le catalogue ...</option>
-                // Boucle for des shop dispo
-                <option value="1">One</option>                
-            </select>            
-            </div>
         </div>
         <button onclick="signup()" id="signupButton" class="w-100 btn btn-primary btn-lg mt-3">
             Modifier
